7c71f0b7fef59acc55728af4dacd22caa52f04b9
Erick Brower <ebrower@quirkyinc.com>
2014-09-29 11:32:47 -0700

    Removed bower generated components from public

--- db/index.js
+++
@@ -1,1 +0,0 @@
-var env = process.env.NODE_ENV || 'development'; var config = require('./conf')[env], Schema = require('jugglingdb').Schema, schema = new Schema('postgres', config); module.exports = schema;
