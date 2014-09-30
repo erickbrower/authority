0339992948acb8f56f6289965cc0a35da9d2768d
Erick Brower <cerickbrower@gmail.com>
2014-09-30 08:46:42 -0700

    Stubbed layout for users

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ var express = require('express'),
   //favicon = require('serve-favicon'),
   oauthserver = require('oauth2-server'),
   cookieParser = require('cookie-parser'),
-  lessMiddleware = require('less-middleware'),
   Handlebars = require('handlebars'),
   router = require('../config/routes'),
   path = require('path'),
@@ -14,7 +13,9 @@ var express = require('express'),
 
 app.use(logger('short'));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 
 app.use(methodOverride());
@@ -30,19 +31,40 @@ var partials = path.join(__dirname, 'views', 'partials');
 fs.readdirSync(partials).forEach(function(file) {
   var source = fs.readFileSync(partials + '/' + file, "utf8"),
     partial = /(.+)\.html/.exec(file).pop();
-
-    console.log(partial);
   Handlebars.registerPartial(partial, source);
 });
 
-var publicPath = path.join(__dirname, '..', 'public');
 
-app.use(lessMiddleware(publicPath));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(express.static(publicPath));
-
-app.use(function(req, res) {
-  res.status(404);
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (process.env.NODE_ENV === 'development') {
+  app.use(function(err, req, res) {
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 
 module.exports = app;
